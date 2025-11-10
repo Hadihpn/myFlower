3971d0e3cd657f1b0840e71397d6af675b7b1139
persnb <persnb@gmail.com>
2025-11-10 06:47:55 +0330

    create plant dto

--- src/modules/plants/dto/create-plant.dto.ts
+++ src/modules/plants/dto/create-plant.dto.ts
@@ -1,1 +1,30 @@
-export class CreatePlantDto {}
+import { IsString, IsDateString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePlantDto {
+  @ApiProperty({ example: 'سانسوریا' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: 'مهتابی' })
+  @IsString()
+  species: string;
+
+  @ApiProperty({ example: 'توضیحات', required: false })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiProperty({ example: 'محل نگهداری', required: false })
+  @IsOptional()
+  @IsString()
+  location?: string;
+
+  @ApiProperty({ example: 'تاریخ کاشت' })
+  @IsDateString()
+  plantedDate: string;
+
+  @ApiProperty()
+  @IsString()
+  deviceId: string;
+}
